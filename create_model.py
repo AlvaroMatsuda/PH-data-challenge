e6e0e072567be726c02987ceb82ba6f458622cf6
AlvaroDhauz <alvaro.matsuda@dhauz.com>
2025-10-04 19:59:16 -0300

    added model performance metrics code

--- create_model.py
+++ create_model.py
@@ -5,11 +5,13 @@ from typing import List
 from typing import Tuple
 
 import pandas
+import numpy as np
 from sklearn import model_selection
 from sklearn import neighbors
 from sklearn import pipeline
 from sklearn import preprocessing
 from sklearn import tree
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, r2_score
 
 SALES_PATH = "data/kc_house_data.csv"  # path to CSV with home sale data
 DEMOGRAPHICS_PATH = "data/zipcode_demographics.csv"  # path to CSV with demographics
@@ -55,9 +57,15 @@ def load_data(
 
 def main():
     """Load data, train model, and export artifacts."""
+    
+    # Loading data
     x, y = load_data(SALES_PATH, DEMOGRAPHICS_PATH, SALES_COLUMN_SELECTION)
+
+    # Train-Test Split
     x_train, _x_test, y_train, _y_test = model_selection.train_test_split(
         x, y, random_state=42)
+
+    # Train model
 
     # model = (
     #     pipeline.make_pipeline(
@@ -76,12 +84,31 @@ def main():
         .fit(x_train, y_train)
         )
 
+    # Model Performance
+    y_pred = model.predict(_x_test)
+
+    mape = float(mean_absolute_percentage_error(_y_test, y_pred))
+    rmse = float(np.sqrt(mean_squared_error(_y_test, y_pred)))
+    r2 = float(r2_score(_y_test, y_pred))
+    metrics = {"mape": mape, "rmse": rmse, "r2": r2}
+
+
+
+    # Saving Artifacts
     output_dir = pathlib.Path(OUTPUT_DIR)
     output_dir.mkdir(exist_ok=True)
 
     # Output model artifacts: pickled model and JSON list of features
     pickle.dump(model, open(output_dir / "model.pkl", 'wb'))
     json.dump(list(x_train.columns), open(output_dir / "model_features.json", 'w'))
+    json.dump(metrics, open(output_dir / "model_metrics.json", 'w'))
+
+    # Printing for Sanity Check
+    print("Model name:")
+    print(model[1])
+
+    print("Model Metrics:")
+    print(metrics)
 
 
 if __name__ == "__main__":
