0080fb3d47985348f2b21db00435e3a511d30af8
AlvaroDhauz <alvaro.matsuda@dhauz.com>
2025-10-09 18:15:58 -0300

    added MAE model performance metric to track

--- create_model.py
+++ create_model.py
@@ -12,7 +12,7 @@ from sklearn import neighbors
 from sklearn.pipeline import Pipeline
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, r2_score, mean_absolute_error
 
 
 # ==========================
@@ -133,7 +133,8 @@ def evaluate_model(
     mape = float(mean_absolute_percentage_error(y_test, y_pred))
     rmse = float(np.sqrt(mean_squared_error(y_test, y_pred)))
     r2 = float(r2_score(y_test, y_pred))
-    metrics = {"mape": mape, "rmse": rmse, "r2": r2}
+    mae  = mean_absolute_error(y_test, y_pred)
+    metrics = {"mape": mape, "rmse": rmse, "r2": r2, "mae": mae}
 
     return metrics
 
